2fb5217c8a4f4c44199e1f9e151b9727439ea902
EduardoLavall <eduardolavalldias@gmail.com>
2024-03-12 10:42:25 -0300

    Código comentado e README
    -Adicionado comentários nos métodos do código
    -Criação do arquivo README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # compressorPDF
  Converte html para pdf e comprime o arquivo
+ - recebe uma url e valida ela
+ - define o nome do arquivo resultante
+ - faz o download do arquivo bruto
+ - comprime o arquivo utilizando GhostScript
+
+## requisitos do sistema
+O sistema utiliza das seguintes bibliotecas:
+- requests
+- subprocess
+- validators
+*Requer instalação do GhostScript no ambiente virtual*
+
+## como usar o compressorPDF
+1. Rode o arquivo "compressor.py" via terminal
+```bash
+python ./compressor.py
+```
+2. Digite a url da página html
+3. Digite o nome desejado para o arquivo
+4. Os arquivos vão estar disponibilizados na pasta raiz do programa
+
+
+
+
+
--- compressor.py
+++ compressor.py
@@ -4,6 +4,7 @@ import validators
 
 #compressorPDF
 def validar_url():
+    #valida a url digitada
     while True:
         url=input("Digite a url de download do arquivo html: ")
         if validators.url(url):
@@ -12,9 +13,10 @@ def validar_url():
     return url
 
 def baixar_arquivo(url, arquivoBruto):
-    apiZorgen = "https://htmltopdf.hcc.app.br/?url="
-    response = requests.get(apiZorgen + url)
+    #envia a url como parâmetro para a API e baixa o arquivo PDF bruto
+    api = "https://htmltopdf.hcc.app.br/?url="
     print("Baixando o pdf...")
+    response = requests.get(api + url)
     if response.status_code == 200:
         with open(arquivoBruto+'.pdf', 'wb') as f:
             f.write(response.content)
@@ -23,24 +25,33 @@ def baixar_arquivo(url, arquivoBruto):
         print("Falha ao baixar o arquivo:", response.status_code)
 
 def comprimir_arquivo_pdf(arquivoBruto):
+    #comprime o arquivo utilizando ghostscript
     
     diretorioGS = r'C:\Program Files\gs\gs10.03.0\bin\gswin64'
     arquivoComprimido = arquivoBruto+"-comprimido.pdf"
     command = '{} -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -sNAME=setting -sOutputFile={} {}'.format(diretorioGS, arquivoComprimido, arquivoBruto+'.pdf')
     print (command)
+    print("Comprimindo o arquivo PDF")
     try:
         subprocess.run(command, shell=False, check=True)
         print("Arquivo PDF comprimido gerado com sucesso!")
     except subprocess.CalledProcessError as e:
         print("Ocorreu um erro ao comprimir o arquivo PDF:", e)
 
-#url = r'https://htmltopdf.hcc.app.br/?url=https://hcc-prd.s3.amazonaws.com/IDF/158582/Orçamento/Proposta/Proposta_IDF_158582_V1.1.html'
+#url = r'https://hcc-prd.s3.amazonaws.com/IDF/158582/Orçamento/Proposta/Proposta_IDF_158582_V1.1.html'
+def main():
+    url = validar_url()
+    arquivoBruto = input('digite o nome do arquivo desejado (exemplo "documento"): ')
+    baixar_arquivo(url, arquivoBruto)
+    comprimir_arquivo_pdf(arquivoBruto)
+    print ('Fim')
+    
+if __name__ == "__main__":
+    main()
+    
 
-url = validar_url()
-arquivoBruto = input('digite o nome do arquivo desejado (exemplo "documento"): ')
-baixar_arquivo(url, arquivoBruto)
-comprimir_arquivo_pdf(arquivoBruto)
-print ('Fim')
+
+
 
 
 
